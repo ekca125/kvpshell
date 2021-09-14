840a5c9167af59f5b81751a261766ac2d2aed1c0
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-09-14 22:09:26 +0900

    Update MainPage.vue

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,13 +1,14 @@
 <template>
   <q-page class="flex flex-center">
-    <div>
-      <div class="left">
-        <PluginTableComponent />
-        
-      </div>
-      <div class="right">
-        <KeyValueTableComponent />
-      </div>
+    <div class="q-pa-md">
+      <q-table
+        title="Plugin"
+        :rows="plugin_datas"
+        :columns="plugin_table_columns"
+        row-key="name"
+      >
+      
+      </q-table>
     </div>
   </q-page>
 </template>
@@ -15,8 +16,54 @@
 <script>
 import { defineComponent } from "vue";
 
+const stub_plugin_datas = [
+  {
+    index: 0,
+    plugin_name: "plugin_preset",
+    plugin_exec: "plugin_exe",
+    plugin_key_value: [
+      {
+        key: "default key1",
+        value: "default key value1",
+      },
+      {
+        key: "default key2",
+        value: "default key value2",
+      },
+    ],
+  },
+  {
+    index: 1,
+    plugin_name: "plugin_preset",
+    plugin_exec: "plugin_exe",
+    plugin_key_value: [
+      {
+        key: "default key4",
+        value: "default key value1",
+      },
+      {
+        key: "default key5",
+        value: "default key value2",
+      },
+    ],
+  },
+];
+
+const stub_plugin_pos = 0;
+
+const plugin_table_columns = [
+  { name: "plugin_name", label: "plugin_name", field: "plugin_name"},
+];
+
 export default defineComponent({
-  name: "MainPage"
+  name: "MainPage",
+  setup() {
+    return {
+      plugin_datas:stub_plugin_datas,
+      plugin_pos:stub_plugin_pos,
+      plugin_table_columns
+    };
+  },
 });
 </script>
 
